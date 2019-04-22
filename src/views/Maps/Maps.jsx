d64229e98987f9d9c112d4718e3f41f6692d157c
Neil Ricci <iccir919@gmail.com>
2019-04-21 21:17:11 -0700

    Add react map gl

--- src/views/Maps/Maps.jsx
+++ src/views/Maps/Maps.jsx
@@ -1,38 +1,32 @@
-/// Maps.js
-import React from 'react';
-import DeckGL from '@deck.gl/react';
-import {LineLayer} from '@deck.gl/layers';
-import {StaticMap} from 'react-map-gl';
-
-// Set your mapbox access token here
-const MAPBOX_ACCESS_TOKEN = 'pk.eyJ1IjoibmVpbHZyIiwiYSI6ImNqcjZweWM0OTA2Zms0NHBmbHZ0MnRmdXAifQ.esuWCmOiK2Jn5NT7SUYdJQ';
-
-// Initial viewport settings
-const initialViewState = {
-  longitude: -122.41669,
-  latitude: 37.7853,
-  zoom: 13,
-  pitch: 0,
-  bearing: 0
-};
-
-// Data to be used by the LineLayer
-const data = [{sourcePosition: [-122.41669, 37.7853], targetPosition: [-122.41669, 37.781]}];
+import React from "react";
+import MapGL from 'react-map-gl';
 
 class Maps extends React.Component {
-  render() {
-    const layers = [
-      new LineLayer({id: 'line-layer', data})
-    ];
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      style: 'mapbox://styles/mapbox/light-v9',
+      viewport: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        longitude: -74,
+        latitude: 40.7,
+        zoom: 11,
+        maxZoom: 16
+      }
+    };
+  }
+
+  render() {
     return (
-      <DeckGL
-        initialViewState={initialViewState}
-        controller={true}
-        layers={layers}
-      >
-        <StaticMap mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
-      </DeckGL>
+      <div>
+        <MapGL {...this.state.viewport} 
+          mapboxApiAccessToken={
+            "pk.eyJ1IjoibmVpbHZyIiwiYSI6ImNqcjZweWM0OTA2Zms0NHBmbHZ0MnRmdXAifQ.esuWCmOiK2Jn5NT7SUYdJQ"
+          }
+        mapStyle={this.state.style} />
+      </div>
     );
   }
 }
